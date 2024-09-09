4b7351402c017798ad34c4a84aadb9f268502cc9
MiguelTucto <u201521691@upc.edu.pe>
2024-09-09 18:18:29 -0500

    feat: draw function has been implemented

--- src/components/camera.jsx
+++ src/components/camera.jsx
@@ -1,7 +1,11 @@
 import Webcam from "react-webcam";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
 import {useCallback, useEffect, useRef, useState} from "react";
 import axios from "axios";
 import {gsap} from "gsap";
+import {FaCheckCircle, FaExclamationCircle, FaHeart, FaRegHeart, FaSpinner} from "react-icons/fa";
+import {FaRegFaceSmile} from "react-icons/fa6";
 
 const Camera = () => {
     const webcamRef = useRef(null);
@@ -34,6 +38,7 @@ const Camera = () => {
         height: 480,
         facingMode: "user"
     };
+
 
     const capture = useCallback(async () => {
         if (!isDetectionActive || isRequestPending) return;
@@ -243,8 +248,276 @@ const Camera = () => {
                 videoConstraints={videoConstraints}
                 style={{width: '100%', height: 'auto', position: 'absolute', zIndex: '1'}}
             />
+            <div style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'white',
+                zIndex: '1',
+                opacity: '0.1'
+            }}></div>
+            {isMenuOpen && (
+                <div ref={menuRef} style={menuStyle}>
+                    {userInfo ? (
+                        <p style={menuTitleStyle}>Bienvenido, {userInfo.first_name}</p>
+                    ) : (
+                        <p>No se ha registrado usuario.</p>
+                    )}
+                    <ul style={detectionListStyle}>
+                        {detections.map((detection, index) => (
+                            <li key={index} style={detectionItemStyle}>
+                                {detection.name}
+                            </li>
+                        ))}
+                    </ul>
+                    {loading && (
+                        <div style={waitingStyle}>
+                            <FaSpinner ref={spinnerRef}
+                                       style={{marginRight: '10px', animation: 'spin 1s linear infinite'}}/>
+                            Cargando...
+                        </div>
+                    )}
+                    {newInfoAvailable && detections.length > 0 && (
+                        <div style={loadingStyle}>
+                            <FaExclamationCircle style={{marginRight: '10px'}}/>
+                            Hay nueva información disponible
+                        </div>
+                    )}
+                    {detections.length == 0 && (
+                        <div style={loadingStyle}>
+                            Realiza nuevas detecciones <FaRegFaceSmile style={{marginLeft: '10px'}}/>
+                        </div>
+                    )}
+                    {showNotifications && (
+                        <div>
+                            <div style={nutritionInFoStyle}>
+                                <FaCheckCircle style={{marginRight: '10px'}}/>
+                                Información nutricional cargada
+                            </div>
+                            <div style={loadingStyle}>
+                                <FaExclamationCircle style={{marginRight: '10px'}}/>
+                                Recetas disponibles
+                            </div>
+                        </div>
+                    )}
+                    {showRecipes && (
+                        <div>
+                            <div style={nutritionInFoStyle}>
+                                <FaCheckCircle style={{marginRight: '10px'}}/>
+                                Recetas cargadas
+                            </div>
+                            <div ref={recipeRef} style={recipeContainerStyle}>
+                                <div className={'flex flex-row justify-between items-center'} style={recipeTitleStyle}>
+                                    <h3>{recipes[currentRecipeIndex].title}</h3>
+                                    <FaRegHeart
+                                        className="favorite-icon"
+                                    />
+                                </div>
+                                <p style={{borderBottom: '1px solid white'}}>
+                                    <strong>Ingredientes:</strong> {recipes[currentRecipeIndex].ingredients}</p>
+                                {showPreparation && (
+                                    <p><strong>Preparación:</strong> {recipes[currentRecipeIndex].preparation}</p>
+                                )}
+                                <span
+                                    style={{marginLeft: '40%'}}>{currentRecipeIndex + 1} de {recipes.length}</span>
+                            </div>
+                        </div>
+
+                    )}
+                    {showFavorites && favoriteRecipes.length > 0 && (
+                        <div style={favoriteContainerStyle}>
+                            <div style={{marginBottom: '20px'}}>
+                                <div className={'flex flex-row justify-between items-center'}
+                                     style={favoriteTitleStyle}>
+                                    <h3>{favoriteRecipes[currentRecipeIndex].title} </h3>
+                                    <FaHeart
+                                        className="favorite-icon"
+                                        style={{color: 'red'}}
+                                    />
+                                </div>
+                                <p><strong>Ingredientes:</strong> {favoriteRecipes[currentRecipeIndex].ingredients}</p>
+                                <p><strong>Preparación:</strong> {favoriteRecipes[currentRecipeIndex].preparation}</p>
+                                <span
+                                    style={{marginLeft: '40%'}}>{currentRecipeIndex + 1} de {favoriteRecipes.length}</span>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            )}
+            {showUserModal && (
+                <div className="modal-container">
+                    <div className={'modal-content'}>
+                        <h2>Ingrese su información</h2>
+                        <input
+                            id="nameInput"
+                            type="text"
+                            value={name}
+                            className="modal-input"
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Nombre completo"
+                        />
+                        <input
+                            id="dobInput"
+                            type="date"
+                            value={dob}
+                            className="modal-input"
+                            onChange={(e) => setDob(e.target.value)}
+                            placeholder="Fecha de nacimiento"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Preferencias alimentarias"
+                            value={foodPreference}
+                            onChange={(e) => setFoodPreference(e.target.value.split(',').map(item => item.trim()))}
+                            className="modal-input"
+                            id="foodPreferenceInput"
+                        />
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
+
+const waitingStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: '16px',
+    margin: '10px 0',
+    color: 'rgb(12, 77, 83)', // Mantener color amarillo para los estados de carga
+    backgroundColor: 'rgb(230, 240, 234)', // Fondo translúcido para mayor contraste
+    padding: '5px',
+    borderRadius: '5px'
+}
+
+const nutritionInFoStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: '16px',
+    margin: '10px 0',
+    color: 'rgb(12, 77, 83)', // Mantener color amarillo para los estados de carga
+    backgroundColor: 'rgb(39, 219, 105)', // Fondo translúcido para mayor contraste
+    padding: '5px',
+    borderRadius: '5px'
+}
+
+const favoriteContainerStyle = {
+    color: 'white',
+    marginTop: '20px',
+    fontSize: '16px',
+    lineHeight: '1.5',
+    border: '1px solid rgba(255, 255, 255, 0.5)', // Borde blanco translúcido para mantener coherencia
+    borderRadius: '10px',
+    padding: '10px',
+    backgroundColor: 'rgba(255, 255, 255, 0.15)'
+}
+
+const favoriteTitleStyle = {
+    margin: '0 0 10px',
+    fontSize: '20px',
+    borderBottom: '1px solid white',
+    paddingBottom: '5px',
+    color: 'white'
+}
+const menuStyle = {
+    position: 'absolute',
+    top: '50px',
+    right: '50px',
+    width: '350px',
+    backgroundColor: 'rgba(18, 119, 128, 0.95)', // Fondo anaranjado más vibrante
+    color: 'white', // Color de texto blanco para contraste
+    padding: '20px',
+    borderRadius: '10px',
+    zIndex: '3',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    fontFamily: 'Arial, sans-serif',
+    opacity: 0,
+    transform: 'translateX(100%)'
+};
+
+const menuTitleStyle = {
+    margin: '0 0 10px',
+    fontSize: '24px',
+    borderBottom: '2px solid white',
+    paddingBottom: '5px',
+    color: 'white' // Color de texto blanco para el título
+};
+
+const detectionListStyle = {
+    listStyleType: 'none',
+    padding: '0',
+    margin: '0',
+    color: 'white' // Color de texto blanco para los items de detección
+};
+
+const detectionItemStyle = {
+    marginBottom: '10px',
+    fontSize: '18px',
+    lineHeight: '1.5',
+    border: '1px solid rgba(255, 255, 255, 0.5)', // Borde blanco translúcido para mantener coherencia
+    borderRadius: '5px',
+    padding: '5px 10px',
+    backgroundColor: 'rgba(255, 255, 255, 0.15)', // Fondo translúcido para detecciones
+    color: 'white' // Color de texto blanco
+};
+
+const recipeContainerStyle = {
+    marginTop: '20px',
+    fontSize: '16px',
+    lineHeight: '1.5',
+    border: '1px solid rgba(255, 255, 255, 0.5)', // Borde blanco translúcido para mantener coherencia
+    borderRadius: '10px',
+    padding: '10px',
+    backgroundColor: 'rgba(255, 255, 255, 0.15)', // Fondo translúcido
+    color: 'white', // Texto blanco
+    opacity: 0
+};
+
+const recipeTitleStyle = {
+    margin: '0 0 10px',
+    fontSize: '20px',
+    borderBottom: '1px solid white',
+    paddingBottom: '5px',
+    color: 'white' // Texto blanco para el título de la receta
+};
+
+const buttonContainerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginTop: '10px'
+};
+
+const buttonStyle = {
+    backgroundColor: 'rgba(255, 140, 0, 0.8)', // Fondo anaranjado translúcido
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    padding: '10px 20px',
+    cursor: 'pointer',
+    fontSize: '16px',
+    transition: 'background-color 0.3s',
+    outline: 'none'
+};
+
+const loadingStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: '16px',
+    margin: '10px 0',
+    color: 'rgb(12, 77, 83)', // Mantener color amarillo para los estados de carga
+    backgroundColor: 'rgb(252, 165, 3)', // Fondo translúcido para mayor contraste
+    padding: '5px',
+    borderRadius: '5px'
+};
+
+const transcriptStyle = {
+    position: 'absolute',
+    bottom: '10px',
+    left: '10px',
+    color: 'white',
+    zIndex: '2'
+};
 
 export default Camera
